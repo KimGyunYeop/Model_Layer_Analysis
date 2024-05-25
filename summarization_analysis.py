98a7482c74b2271c11bef53af136dad89e7d3b74
KimGyunYeop <>
2024-05-25 19:02:52 +0900

    auto push

--- summarization_analysis.py
+++ summarization_analysis.py
@@ -88,11 +88,11 @@ print(data)
 # data_path = "meta-llama/Llama-2-70b-hf"
 # data_path = "google/gemma-7b"
 # data_path = "meta-llama/Meta-Llama-3-8B"
-data_path = "meta-llama/Meta-Llama-3-8B-Instruct"
+# data_path = "meta-llama/Meta-Llama-3-8B-Instruct"
 # data_path = "meta-llama/Llama-2-7b-chat-hf"
 # data_path = "meta-llama/Meta-Llama-Guard-2-8B"
 data_path = args.model_path
-quantization_config = BitsAndBytesConfig(load_in_4bit=True, llm_int8_enable_fp32_cpu_offload=True)
+# quantization_config = BitsAndBytesConfig(load_in_4bit=True, llm_int8_enable_fp32_cpu_offload=True)
 # model = AutoModelForCausalLM.from_pretrained(data_path, torch_dtype=torch.bfloat16, device_map=device, cache_dir="./cache")
 model = AutoModelForCausalLM.from_pretrained(data_path, quantization_config=quantization_config, device_map=device, cache_dir="./cache")
 print(model.device)
