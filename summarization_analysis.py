bda0dfa135f705428cf7704c4a4a445708c01aec
KimGyunYeop <>
2024-05-25 13:16:57 +0900

    auto push

--- summarization_analysis.py
+++ summarization_analysis.py
@@ -78,9 +78,11 @@ tokenizer = AutoTokenizer.from_pretrained(data_path)
 print(model)
 print(tokenizer)
 
-new_line_token = tokenizer("\n")['input_ids'][-1]
-new_line_token2 = tokenizer(" \n")['input_ids'][-1]
-stopping_ids = [tokenizer.eos_token_id, new_line_token, new_line_token2]
+stopping_ids = [tokenizer.eos_token_id]
+# new_line_token = tokenizer("\n")['input_ids'][-1]
+# stopping_ids.append(new_line_token)
+# new_line_token2 = tokenizer(" \n")['input_ids'][-1]
+# stopping_ids.append(new_line_token2)
 if "Llama-3" in data_path:
     stopping_ids.append(tokenizer.convert_tokens_to_ids("<|eot_id|>"))
 gen_args = {'do_sample': True, 'top_k': 10, 'num_return_sequences': 1, 'eos_token_id': stopping_ids, 'max_new_tokens': 300, 'pad_token_id': tokenizer.eos_token_id, "early_stopping":True, "temperature":0.7}
